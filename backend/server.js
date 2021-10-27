68fa8390cdc08d7ef7d1f120b180516145df9cae
Oluwatoyin Jolaoso <admin@Oluwatoyins-MacBook-Pro.local>
2021-10-27 02:05:29 +0100

    Setting Up backend & frontend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const products = require('./data/products')
+const dotenv = require('dotenv')
+const app = express()
+
+dotenv.config()
+
+app.get('/', (req, res) => {
+  res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id == req.params.id)
+  res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(5000, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}!`)
+})
